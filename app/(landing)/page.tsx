badacb8b7cf0bb33aebac1f1f0bf1038cc0f5768
unknown <1242002059@student.bakrie.ac.id>
2026-01-13 23:59:48 +0700

    Task Live Session 1 - Slicing Home Page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,12 @@
+import HeroSection from "./components/homes/hero";
+import CategoriesSection from "./components/homes/categories";
+import ProductSection from "./components/homes/products";
+
+export default function Home() {
+  return <main>
+    <HeroSection />
+    <CategoriesSection />
+    <ProductSection />
+
+  </main>
+}
